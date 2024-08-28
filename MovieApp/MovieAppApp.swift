e4cab0a4965fdb3aa8ef58c3307afa64d7ff53ac
Benjamin <benjaminbelloeil03@gmail.com>
2024-08-28 17:13:12 -0600

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Benjamin Belloeil on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
