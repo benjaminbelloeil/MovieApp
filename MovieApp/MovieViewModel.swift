fb5e6e085ca770c0e1ff62063682bae051aab069
Benjamin <benjaminbelloeil03@gmail.com>
2024-08-28 20:35:10 -0600

    feat: Add almost complete SwiftUI views and models for MovieApp

---
+++ MovieApp/MovieViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MovieViewModel.swift
+//  MovieApp
+//
+//  Created by Benjamin Belloeil on 8/28/24.
+//
+
+import SwiftUI
+
+struct MovieViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieViewModel()
+}
